2a184ba770b302d76e4ceca045034f38c462dd10
Mukesh Dani <mukeshdani00@gmail.com>
2022-10-19 19:37:07 +0530

    Index.js file with code and explanation and add all Sorting Algo

--- Sorting_Visualizer/index.js
+++ Sorting_Visualizer/index.js
@@ -3,6 +3,7 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
+  
   // Navigation Bar Dropdown Swapping
   let A = document.getElementsByClassName("dropdown-item");
   let curAlgo = "Bubble Sort";
@@ -347,5 +348,6 @@ function randomNumber(min, max) {
     quickSort(j + 1, end);
   }
   
+
   generateNewArray();
   
